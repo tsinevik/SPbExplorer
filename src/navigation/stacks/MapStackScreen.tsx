5b9aeb0c7b50e364ebd600f6903502d273f3e29c
romantsinevich <braid98@yandex.ru>
2021-04-19 19:57:29 +0300

    Hide header for map screen

--- src/navigation/stacks/MapStackScreen.tsx
+++ src/navigation/stacks/MapStackScreen.tsx
@@ -5,7 +5,7 @@ import { MapScreen } from 'screens/MapScreen';
 const MapStack = createStackNavigator();
 
 export const MapStackScreen = () => (
-  <MapStack.Navigator>
+  <MapStack.Navigator headerMode={'none'}>
     <MapStack.Screen name="Map" component={MapScreen} />
   </MapStack.Navigator>
 );
