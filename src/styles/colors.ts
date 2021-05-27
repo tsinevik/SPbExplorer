101e223374ff474c0d178c63d31933d95375662d
romantsinevich <braid98@yandex.ru>
2021-05-27 16:51:47 +0300

    Add typography and colors styles

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  green: '#5E776B',
+  brown: '#886B57',
+  black: '#0D1821',
+  white: '#F5F5F5',
+  gray: '#A2A4AF',
+  alabaster: '#F2ECE3',
+  beige: '#E8C087',
+};
---
+++ src/styles/typography.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const typography = StyleSheet.create({
+  regularText: {
+    fontFamily: 'Gill Sans',
+  },
+  headingText: {
+    fontFamily: 'Montserrat',
+  },
+});
