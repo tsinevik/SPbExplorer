21ccbc51e3d6a917ea3db9aa56dc38ae25937ccb
romantsinevich <braid98@yandex.ru>
2021-05-31 18:17:27 +0300

    Update profile screen

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,21 +1,31 @@
 import * as React from 'react';
+import * as Progress from 'react-native-progress';
 import {
-  Body,
   Button,
   Card,
   CardItem,
   Container,
   Content,
   H1,
-  Image,
-  Left,
+  Right,
   Text,
   View,
 } from 'native-base';
+import Achievement from 'components/Achievement';
+import Friend from 'components/Friend';
+import UserAvatar from 'react-native-user-avatar';
+import { colors } from 'styles/colors';
+import { StyleSheet } from 'react-native';
+import { typography } from 'styles/typography';
+
+const styles = StyleSheet.create({
+  progress: { fontFamily: typography.bigNumbersFont },
+});
 
 export const ProfileScreen = ({ navigation }) => {
   const user = {
     name: 'Roman Tsinevich',
+    imageUrl: '',
     level: 5,
     completedQuests: 3,
     experience: 1300,
@@ -23,15 +33,32 @@ export const ProfileScreen = ({ navigation }) => {
     cityKnowledge: 43,
   };
 
+  const friends = [
+    { name: 'Vasily Kekich', imageUrl: '' },
+    { name: 'Viktor Timokhov', imageUrl: '' },
+  ];
+
   return (
     <Container>
       <Content>
-        {/*<Image source={{ uri: '' }} />*/}
+        <UserAvatar size={60} name={user.name} src={user.imageUrl} />
         <H1>{user.name}</H1>
         <H1>Уровень</H1>
         <View>
           <H1>{user.level}</H1>
         </View>
+        <Progress.Circle
+          size={90}
+          progress={0.5}
+          thickness={3}
+          showsText={true}
+          formatText={() => user.level}
+          color={colors.progress}
+          unfilledColor={colors.gray}
+          borderColor={'rgba(0,0,0,0)'}
+          fill={colors.bgSecondary}
+          textStyle={styles.progress}
+        />
         <H1>Статистика</H1>
         <Card>
           <CardItem style={{ borderWidth: 2 }}>
@@ -53,48 +80,24 @@ export const ProfileScreen = ({ navigation }) => {
         </Card>
         <H1>Друзья</H1>
         <Card>
-          <CardItem style={{ borderWidth: 2 }}>
-            <Left>{/*<Image source={{ uri: '' }} />*/}</Left>
-            <Body>
-              <Text>Vasily Kekich</Text>
-            </Body>
-          </CardItem>
-          <CardItem style={{ borderWidth: 2 }}>
-            <Left>{/*<Image source={{ uri: '' }} />*/}</Left>
-            <Body>
-              <Text>Vasily Kekich</Text>
-            </Body>
-          </CardItem>
+          <Friend name={friends[0].name} imageUrl={friends[0].imageUrl} />
+          <Friend name={friends[1].name} imageUrl={friends[1].imageUrl} />
           <CardItem>
-            <Button>
+            <Button bordered>
               <Text>Все</Text>
             </Button>
           </CardItem>
         </Card>
         <H1>Достижения</H1>
         <Card>
-          <CardItem style={{ borderWidth: 2 }}>
-            <Left>{/*<Image source={{ uri: '' }} />*/}</Left>
-            <Body>
-              <Text>Пройти 4 квеста</Text>
-              <View style={{ borderWidth: 2, borderRadius: 10 }}>
-                <Text>2/4</Text>
-              </View>
-            </Body>
-          </CardItem>
-          <CardItem style={{ borderWidth: 2 }}>
-            <Left>{/*<Image source={{ uri: '' }} />*/}</Left>
-            <Body>
-              <Text>Пройти 4 квеста</Text>
-              <View style={{ borderWidth: 2, borderRadius: 10 }}>
-                <Text>2/4</Text>
-              </View>
-            </Body>
-          </CardItem>
+          <Achievement task={'Пройти 4 квеста'} total={4} progress={2} />
+          <Achievement task={'Пройти 4 квеста'} total={4} progress={2} />
           <CardItem>
-            <Button>
-              <Text>Все</Text>
-            </Button>
+            <Right>
+              <Button bordered>
+                <Text>Все</Text>
+              </Button>
+            </Right>
           </CardItem>
         </Card>
       </Content>
